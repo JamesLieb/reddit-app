551b9e3b96234af300c76a24f4e93e9aa078ca7f
James <james_lieb@hotmail.com>
2025-10-18 13:25:18 -0400

    Created jsx for the header component and removed the footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 import { Header } from './components/Header.jsx'
 import { Subreddits } from './components/Subreddits.jsx'
 import { PopularCard} from './components/PopularCard.jsx'
-import { Footer } from './components/Footer.jsx'
+
 
 
 function App() {
   return (
-    <div>
+    <div >
       <Header />
       <Subreddits />
       <PopularCard />
-      <Footer />
+      
     </div>
   )
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import RedditLogo from '../Reddit_Logo.png';
 
 export const Header = () => {
-    return <div>Header</div>
+    return (
+        <div className=" items-baseline flex-col border-b-2 border-gray-200 p-4 justify-center">
+            <div>
+                <img className="aspect 3/2 object-cover" src={RedditLogo} alt="Logo" height={200} width={200} />
+            </div>
+            <div className=" -mt-6 text-4xl font-bold text-black-500 text-center"> 
+                Reddit App
+            </div>
+            
+        </div>
+    )
 };
